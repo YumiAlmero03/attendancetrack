90ffff40c436f083a99fb61c62d92e19f9d5d1be
Lanie Almero <lanialmero@gmail.com>
2022-03-16 20:20:05 -0400

    edit db

--- inc/env.php
+++ inc/env.php
@@ -3,4 +3,4 @@
 $dbServername = "aase00cbfhf9yn.cecs2ncptkar.ap-southeast-1.rds.amazonaws.com";
 $dbUsername = "atdncmonitoring";
 $dbPassword = "6PBcFtp#PJ1pyfHUNiil";
-$dbName = "aase00cbfhf9yn";
+$dbName = "ebdb";
