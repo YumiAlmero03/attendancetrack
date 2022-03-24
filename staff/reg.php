30be5c90b43da46354ea8d0c3f84388943b37050
Lanie Almero <lanialmero@gmail.com>
2022-03-24 04:05:39 -0400

    test 1

--- staff/reg.php
+++ staff/reg.php
@@ -17,58 +17,10 @@ $pnum = mysqli_real_escape_string($conn, $_POST['pnum']);
 $type = mysqli_real_escape_string($conn, $_POST['type']);
 $studid = mysqli_real_escape_string($conn, $_POST['studid']);
 
-// photoupload
-$target_dir = "../uploads/profile/";
-$target_file = $target_dir . basename($_FILES["photo"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-// // Check if image file is a actual image or fake image
-// if(isset($_POST["submit"])) {
-//   $check = getimagesize($_FILES["photo"]["tmp_name"]);
-//   if($check !== false) {
-//     $msg =  "File is an image - " . $check["mime"] . ".";
-//     $uploadOk = 1;
-//   } else {
-//     $msg =  "File is not an image.";
-//     $uploadOk = 0;
-//   }
-// }
-
-// // Check if file already exists
-// if (file_exists($target_file)) {
-//   $msg =  "Sorry, file already exists.";
-//   $uploadOk = 0;
-// }
-
-// // Check file size
-// if ($_FILES["photo"]["size"] > 500000) {
-//   $msg =  "Sorry, your file is too large.";
-//   $uploadOk = 0;
-// }
-
-// // Allow certain file formats
-// if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-// && $imageFileType != "gif" ) {
-//   $msg =  "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-//   $uploadOk = 0;
-// }
-
-// // Check if $uploadOk is set to 0 by an error
-// if ($uploadOk == 0) {
-//   // $msg =  "Sorry, your file was not uploaded.";
-// // if everything is ok, try to upload file
-// } else {
-//   if (move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)) {
-//     $msg =  "The file ". htmlspecialchars( basename( $_FILES["photo"]["name"])). " has been uploaded.";
-//   } else {
-//     $msg = "Sorry, there was an error uploading your file.";
-//   }
-// }
-// upload end
+// upload 
   $filename = uploadS3($_FILES["photo"]["name"],$_FILES["photo"]["tmp_name"]);
 
-
+var_dump($_FILES);
 $insert_code = "INSERT INTO `registered`(`qrcode`, `firstname`, `lastname`, `type`, `course`, `year`, `section`, `bday`, `email`, `address`, `pname`, `pcontact`, `photo`, `qrphoto`) 
 VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 
