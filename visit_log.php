8f34e80c536365cd456bd999335d71bd2b71d0eb
Lanie Almero <lanialmero@gmail.com>
2022-03-24 04:55:19 -0400

    visit link

--- visit_log.php
+++ visit_log.php
@@ -21,7 +21,30 @@ $img = $_POST['image'];
   
     $image_base64 = base64_decode($image_parts[1]);
     $fileName = uniqid() . '.png';
-  
+function uploadVisitIMG($name,$tmpname)
+{
+    // var_dump($file);
+    $file_name = 'files/'.$name;   
+    $temp_file_location = $tmpname; 
+
+    require 'inc/vendor/autoload.php';
+
+    $s3 = new Aws\S3\S3Client([
+        'region'  => 'ap-southeast-1',
+        'version' => 'latest',
+        'credentials' => [
+            'key'    => "AKIAVZ4H6LJO4JMRJISB",
+            'secret' => "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",
+        ]
+    ]);     
+
+    $result = $s3->putObject([
+        'Bucket' => 'onsitelogbook-files',
+        'Key'    => $file_name,
+        'SourceFile' => $temp_file_location         
+    ]);
+    return $result['ObjectURL'];
+}
     $file = uploadS3($fileName,$image_base64);
     
 
