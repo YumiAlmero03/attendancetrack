3f4acfca8ffcfd08d48f5539daa560cd51792f00
Lanie Almero <lanialmero@gmail.com>
2022-03-28 13:54:34 -0400

    fix local db

--- inc/env.php
+++ inc/env.php
@@ -1,6 +1,6 @@
 <?php
 
-$dbServername = "aase00cbfhf9yn.cecs2ncptkar.ap-southeast-1.rds.amazonaws.com";
-$dbUsername = "atdncmonitoring";
-$dbPassword = "6PBcFtp#PJ1pyfHUNiil";
-$dbName = "ebdb";
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "db_attendance";
